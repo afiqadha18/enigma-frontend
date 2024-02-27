dbec8ce6252df0ac7b4541d2e45120c82b196b0f
Akram Hakim <akramhakim621@gmail.com>
2024-02-27 20:37:47 +0800

    commit to pull

--- src/app/pages/bgp-peering/excel-upload/excel-upload.component.ts
+++ src/app/pages/bgp-peering/excel-upload/excel-upload.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
 import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { BgpPeeringService } from 'src/app/services/bgp-peering.service';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'excel-upload-cmp',
@@ -10,8 +13,14 @@ import Swal from 'sweetalert2';
 
 export class ExcelUploadComponent{
 
+  constructor(private bgpService: BgpPeeringService, private router: Router) { }
+
   public files: Array<any>= [];
+  public physicalFile: Array<any>= [];
   fileType: Array<String> = ['text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+  data = {
+    uploadedBy: 0, uploadedOn: '', remarks: ''
+  }
 
   public dropped(files: NgxFileDropEntry[]) {
     for (const droppedFile of files) {
@@ -25,6 +34,7 @@ export class ExcelUploadComponent{
 
           if (this.files.length == 0) {
             if (this.fileType.includes(file.type)) {
+              this.physicalFile.push(file);
               this.files.push({
                   filename: file.name,
                   filetype: file.type,
@@ -85,5 +95,86 @@ export class ExcelUploadComponent{
 
   public onRemove() {
     this.files.pop();
+  }
+
+  public onUpload() {
+    this.data.uploadedBy = 1;
+    this.data.uploadedOn = moment(new Date()).format('YYYY-MM-DD');
+    this.addRemarks();
+  }
+
+  private addRemarks() {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Please insert remarks!",
+      icon: "warning",
+      input: 'textarea',
+      showCancelButton: true,
+      confirmButtonText: "Submit",
+      cancelButtonText: "Cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.data.remarks = result.value;
+        this.submitFormData(this.data);
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Submission is not proceed!",
+          icon: "error"
+        });
+      }
+    });
+  }
+
+  submitFormData(data: any) {
+    let formdata = new FormData();
+    let timerInterval;
+    console.log(this.physicalFile[0]);
+
+    formdata.append('data', JSON.stringify(data));
+    formdata.append('attachment', this.physicalFile[0]);
+
+    this.bgpService.uploadExcelIp(formdata)
+      .subscribe((res: any) => {
+        console.log(res);
+        if (res.message == 'file uploaded successfully') {
+          Swal.fire({
+            icon: 'success',
+            title: "Submitted",
+            text: "File has been uploaded successfully!",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+          }).then((result) => {
+            if (result.dismiss === Swal.DismissReason.timer) {
+              // this.router.navigate(['/bgp-peering']);
+              window.location.reload();
+            }
+          });
+        } else if (res.message == 'Invalid ip address found! Please fix those ip addresses.') {
+          Swal.fire({
+            title: 'Invalid ip address found!',
+            text: 'Please fix those ip addresses before submitting again',
+            icon: 'error'
+          });
+        } else if (res.message == 'Duplicates IP Address found! Please fix those ip addresses.') {
+          Swal.fire({
+            title: 'Duplicates IP Address found',
+            text: 'Please fix those ip addresses before submitting again',
+            icon: 'error'
+          });
+        }
+
+      })
   }
 }
