1d3db16fe89db30f4d7bc5efca9ac8af7d463399
ikhmalhijjaz97 <ikhmalhijjaz72@gmail.com>
2024-03-23 18:10:54 +0800

    add username to local storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,15 +5,16 @@ import { Router } from "@angular/router";
 @Injectable({providedIn: "root"})
 export class AuthService{
     private token: string | undefined;
+    private username: string | undefined | null;
+    private userId: string | undefined;
     private isUserAuthenticated = false;
     private authStatusListener = new Subject<boolean>();
     private tokenTimer : any;
-    private userId: string | undefined;
     constructor(private http:HttpClient, private router: Router){}
 
     login(username: string, password: string){
         const authData = { username: username, password:password};
-        return this.http.post<{token : string, expiresIn: number, userId: string, firstTimeLogin: boolean}>('http://localhost:3001/api/login',authData)
+        return this.http.post<{token : string, expiresIn: number, userId: string, username: string, firstTimeLogin: boolean}>('http://localhost:3001/api/login',authData)
         .subscribe(response => {
             console.log("response "+ response.token);
             this.token = response.token;
@@ -21,12 +22,13 @@ export class AuthService{
                 const expiresInDuration = response.expiresIn;
                 console.log("expire time :" + expiresInDuration * 1000);
                 this.setAuthTimer(expiresInDuration);
+                this.username = response.username;
                 this.userId = response.userId;
                 this.authStatusListener.next(true);
                 this.isUserAuthenticated = true;
                 const now = new Date();
                 const expirationDate = new Date(now.getTime() + expiresInDuration*1000);
-                this.saveAuthenticationData(this.token, expirationDate,this.userId);
+                this.saveAuthenticationData(this.token, expirationDate,this.userId, this.username);
                 //if first time user flag is up then we need to make user change password
                 if(response.firstTimeLogin){
                     this.router.navigate(['/firstTimeLogin', this.userId]);
@@ -63,11 +65,16 @@ export class AuthService{
                 this.authStatusListener.next(true);
                 this.isUserAuthenticated = true;
                 this.userId = authInformation.userId;
+                this.username = authInformation.username;
             }
     }
 
     getUserId(){
         return this.userId;
+    }
+
+    getUsername(){
+        return this.username;
     }
 
     getAuthStatusListener(){
@@ -85,6 +92,7 @@ export class AuthService{
         clearTimeout(this.tokenTimer);
         this.clearAuthenticationData();
         this.userId = "";
+        this.username ="";
         this.router.navigate(['/login']);
     }
 
@@ -92,29 +100,33 @@ export class AuthService{
         return this.isUserAuthenticated;
     }
 
-    private saveAuthenticationData(token: string, expirationDate: Date, userId: string){
+    private saveAuthenticationData(token: string, expirationDate: Date, userId: string, username: string){
         localStorage.setItem("token", token);
         localStorage.setItem("expiration", expirationDate.toISOString());
         localStorage.setItem("userId", userId);
+        localStorage.setItem("username", username);
     }
 
     private clearAuthenticationData(){
         localStorage.removeItem("token");
         localStorage.removeItem("expiration");
         localStorage.removeItem("userId");
+        localStorage.removeItem("username");
     }
 
     private getAuthenticationData(){
         const token = localStorage.getItem("token");
         const expirationDate = localStorage.getItem("expiration");
         const userId = localStorage.getItem("userId");
+        const username = localStorage.getItem("username");
         if(!token || !expirationDate || !userId){
             return;
         }
         return {
             token: token,
             expirationDate: new Date(expirationDate),
-            userId: userId
+            userId: userId,
+            username: username
         }
     }
 
